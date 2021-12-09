e5bf5c073aaff1082ad94c2c7f5927eb18857c63
celynadaminello <celynadaminello@yahoo.com.br>
2021-12-08 21:10:45 -0300

    login-show-de-imagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
+
+
 function validate(){
 
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
     if(username=="admin"&& password=="1234")
-
     {
         alert("login succesfully");
+        document.getElementById('form').action = 'https://showdeimagem.com.br/';
         return false;
     }
     else{
 
         alert("login failed");
+        document.form.submit();
     }
 
 }
